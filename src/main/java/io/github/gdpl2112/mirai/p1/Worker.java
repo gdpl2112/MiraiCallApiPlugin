5e29a6a8efe98707e67e6da91a8b0b8c7bffca5b
HRS-Computer <3474006766@qq.com>
2022-08-06 20:44:59 +0800

    update

--- src/main/java/io/github/gdpl2112/mirai/p1/Worker.java
+++ src/main/java/io/github/gdpl2112/mirai/p1/Worker.java
@@ -149,6 +149,7 @@ public class Worker {
         try {
             Connection connection = org.jsoup.Jsoup.connect(url).ignoreContentType(true).ignoreHttpErrors(true)
                     .header("Host", new URL(url).getHost())
+                    .header("accept-encoding", "gzip, deflate, br")
                     .userAgent(
                             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50"
                     );
