4a5671e7b0ee9a89b070befcbba52eecc94c7e9d
HRS-Computer <3474006766@qq.com>
2022-08-06 22:00:10 +0800

    update

--- src/main/java/io/github/gdpl2112/mirai/p1/Worker.java
+++ src/main/java/io/github/gdpl2112/mirai/p1/Worker.java
@@ -153,7 +153,7 @@ public class Worker {
                     .userAgent(
                             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50"
                     );
-            connection.timeout(600000);
+            connection.timeout(60000);
             if (template.getProxyIp() != null && template.getProxyPort() != null && !template.getProxyIp().isEmpty()) {
                 connection.proxy(template.getProxyIp(), template.getProxyPort());
             } else if (conf.getProxyIp() != null && conf.getProxyPort() != null && !conf.getProxyIp().isEmpty()) {
